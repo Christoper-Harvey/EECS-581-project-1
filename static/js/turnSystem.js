2a6d1ca13f72578d14285039ae1dae008ee36978
r889s843 <ryan.strong@ku.edu>
2024-09-15 22:41:56 -0500

    comments
    comments on turnsystem and animations

--- static/js/turnSystem.js
+++ static/js/turnSystem.js
@@ -1,47 +1,38 @@
+/**
+ * NAME: Battleship - EECS581 Project 1 - turnSystem
+ * DESCRIPTION: This program controls switching turns in the game of battleship
+ * INPUT: None
+ * OUTPUT: changes UI to reflect a change in player's turn and switches game state to player's turn
+ * SOURCES: None
+ * AUTHORS: Ryan Strong
+ * DATE: 9/11/24
+ */
+
 let turn = 1; //int to track which player's turn it is - alternates between 1 and 2
 let p1 = new Player(1)  //Player with id 1
 let p2 = new Player(2) //Player with id 2
 
-//makes it the next player's turn
+//switches turn to the next player
 function nextTurn() {
     if(turn == 1) {//change to p2's turn
-        turn = 2;
-        document.getElementById("p1self").style.display = "none";
+        turn = 2;//turn tracker now shows p2
+        document.getElementById("p1self").style.display = "none";//hide p1's boards
         document.getElementById("p1opponent").style.display = "none";
 
-        document.getElementById("p2self").style.display = "grid";
+        document.getElementById("p2self").style.display = "grid";//unhide p2's boards
         document.getElementById("p2opponent").style.display = "grid";
 
-        document.getElementById("game-state").innerText = "Player 2's Turn";
+        document.getElementById("game-state").innerText = "Player 2's Turn";//top of page now says it's p2's turn
     } else {//change to p1's turn
-        turn = 1;
-        document.getElementById("p1self").style.display = "grid";
+        turn = 1;//turn tracker to p1
+        document.getElementById("p1self").style.display = "grid";//unhide p1's boards
         document.getElementById("p1opponent").style.display = "grid";
 
-        document.getElementById("p2self").style.display = "none";
+        document.getElementById("p2self").style.display = "none";//hide p2's boards
         document.getElementById("p2opponent").style.display = "none";
 
-        document.getElementById("game-state").innerText = "Player 1's Turn";
+        document.getElementById("game-state").innerText = "Player 1's Turn";//top of page now says its p1's turn
     }
 
     return turn
-}
-
-//text alert function
-function alert(message) {
-    //make message appear
-    document.getElementById("text-alert").textContent = message;
-    document.getElementById("text-alert").style.display = "inline";
-
-    //fadeout
-    setTimeout(() => {
-        document.getElementById("text-alert").style.transition = "all 3s";
-        document.getElementById("text-alert").style.opacity = "0";
-    }, 2000);
-
-    //reset
-    document.getElementById("text-alert").style.transition = "all 0s";
-    document.getElementById("text-alert").style.opacity = "1";
-
-
 }
